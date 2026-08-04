8bf3eedb7c1d1813e6d791e170dc556cf4576c5f
akshay.ts <akshay.ts@finfactortech.onmicrosoft.com>
2026-08-04 23:07:25 +0530

    feat(): Find min and max of an array

---
+++ src/arrays/MaxAndMinArray.java
@@ -0,0 +1,18 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class MaxAndMinArray {
+    public static void findMaxAndMin(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            System.out.println("Array is empty");
+            return;
+        }
+
+        int max = Arrays.stream(arr).max().getAsInt();
+        int min = Arrays.stream(arr).min().getAsInt();
+
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+    }
+}
