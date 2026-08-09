9e0070bbf0a6b9afe663bb1eda89418b48a14ae8
akshay.ts <akshay.ts@finfactortech.onmicrosoft.com>
2026-08-09 19:12:38 +0530

    feat(): Check if two strings are anagrams.

---
+++ src/hashMap/Anagrams.java
@@ -0,0 +1,36 @@
+package hashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Anagrams {
+    // Check if two strings are anagrams
+    public static void checkAnagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            System.out.println("Not anagram");
+            return;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : str1.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : str2.toCharArray()) {
+            if (!map.containsKey(ch)) {
+                System.out.println("Not anagram");
+                return;
+            }
+
+            map.put(ch, map.get(ch) - 1);
+
+            if (map.get(ch) < 0) {
+                System.out.println("Not anagram");
+                return;
+            }
+        }
+
+        System.out.println("Anagram");
+    }
+}
