40a68a1d8066df45ac27f1f8904d06c3b6c536a2
akshay.ts <akshay.ts@finfactortech.onmicrosoft.com>
2026-08-12 21:49:24 +0530

    feat(): Reverse a string using a stack.

---
+++ src/stack/ReverseString.java
@@ -0,0 +1,21 @@
+package stack;
+
+import java.util.Stack;
+
+public class ReverseString {
+    private ReverseString() {}
+
+    // Reverse a string using a stack
+    public static void reverseString(char[] str){
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str){
+            stack.push(ch);
+        }
+
+        for (int i = 0; i < str.length; i++){
+            str[i] = stack.pop();
+        }
+        System.out.print(str);
+    }
+}
