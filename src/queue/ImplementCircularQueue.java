57c98136bf20f711ac1746bd57e796ae37ee1547
akshay.ts <akshay.ts@finfactortech.onmicrosoft.com>
2026-08-20 22:57:41 +0530

    feat(): Implement a circular queue.

---
+++ src/queue/ImplementCircularQueue.java
@@ -0,0 +1,46 @@
+package queue;
+
+public class ImplementCircularQueue {
+    private ImplementCircularQueue() {}
+
+    // Implement a circular queue
+    public static void implementCircularQueue(){
+        int[] queue = new int[10];
+        int front = 0;
+        int rear = 0;
+        int size = 0;
+
+        queue[rear] = 10;
+        rear = (rear + 1) % queue.length;
+        size++;
+
+        queue[rear] = 20;
+        rear = (rear + 1) % queue.length;
+        size++;
+
+        queue[rear] = 30;
+        rear = (rear + 1) % queue.length;
+        size++;
+
+        System.out.println("Queue elements:");
+
+        for(int i = 0; i < size; i++){
+            System.out.println(queue[(front + i) % queue.length]);
+        }
+
+        int removed = queue[front];
+        front = (front + 1) % queue.length;
+        size--;
+
+        System.out.println("Removed elements:" + removed);
+
+        queue[rear] = 40;
+        size++;
+
+        System.out.println("Queue after circular insertion:");
+
+        for (int i = 0; i < size; i++){
+            System.out.println(queue[(front + i) % queue.length]);
+        }
+    }
+}
